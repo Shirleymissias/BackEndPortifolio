d378c413edc589d0bad259ca3790b9512fe540d5
SHIRLEY MISSIAS DE SOUZA <SHIRLEY6038@DIGITALCOLLEGE.LOCAL>
2024-01-22 21:07:39 -0300

    Inserção dos endPoints

--- index.js
+++ index.js
@@ -15,8 +15,18 @@ const homeInfo = {
     info:"Sou boa demias programando é isso"
 }
 
+const aboutInfo = {
+    title: "Frontend Developer & Graphic Designer",
+    paragraphOne: " Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis, quas voluptatum facere quam iusto itaque sapiente dolor provident aspernatur unde, suscipit illum, velit similique animi iste corporis repellendus veritatis minus voluptas. Sunt, eligendi. Praesentium explicabo facere quo ad earum.",
+    paragrapTwo: " Lorem ipsum dolor sit amet consectetur adipisicing elit. At reiciendis, quas voluptatum facere quam iusto itaque sapiente dolor provident."
+}
+
 app.get('/home', (request, response) => {
     response.json(homeInfo)
+})
+
+app.get('/about', (request, response) => {
+    response.json(aboutInfo)
 })
 
 
